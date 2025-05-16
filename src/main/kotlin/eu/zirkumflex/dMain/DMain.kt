4e8190886bbfab005cf89ba2eeb7a2c4c1ead2e9
Lukas Bylina <mail@zirkumflex.dev>
2025-05-16 22:55:17 +0200

    init

---
+++ src/main/kotlin/eu/zirkumflex/dMain/DMain.kt
@@ -0,0 +1,14 @@
+package eu.zirkumflex.dMain
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DMain : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
